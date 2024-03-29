2e5d67b143aa4b5b3459e0ec25500b64572d76c2
Robert Stomski <stomski.robert@gmail.com>
2024-03-29 13:14:50 -0600

    add user logout backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -34,11 +34,16 @@ router.post("/", async (req, res, next) => {
     username: user.username,
   };
 
-  await setTokenCookie(res, safeUser);
+  setTokenCookie(res, safeUser);
 
   return res.json({
     user: safeUser,
   });
+});
+
+router.delete("/", (_req, res) => {
+  res.clearCookie("token");
+  return res.json({ message: "success" });
 });
 
 module.exports = router;
