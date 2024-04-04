5b984de9450ab60235ac99bf59ce8525a2888388
Robert Stomski <stomski.robert@gmail.com>
2024-04-04 14:44:13 -0600

    finished reviews router rd 1.5

--- backend/db/migrations/20240402154448-create-spot-image.js
+++ backend/db/migrations/20240402154448-create-spot-image.js
@@ -20,7 +20,7 @@ module.exports = {
         spotId: {
           type: Sequelize.INTEGER,
           allowNull: false,
-          references: { model: "Spots" },
+          references: { model: "Spots", key: "id" },
           onDelete: "CASCADE",
         },
         url: {
