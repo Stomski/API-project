5b93f5ec5681a95c461d68b3fd2efbd422fa1d81
Robert Stomski <stomski.robert@gmail.com>
2024-04-08 00:19:33 -0600

    push to deploy

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -21,41 +21,6 @@ const {
 const { handleValidationErrors } = require("../../utils/validation");
 const { errorMonitor } = require("mocha/lib/runner");
 
-const queryValidate = [
-  query("page")
-    .optional()
-    .isInt({ min: 1 })
-    .withMessage("Page must be greater than or equal to 1"),
-  query("size")
-    .optional()
-    .isInt({ min: 1 })
-    .withMessage("Page must be greater than or equal to 1"),
-  query("maxLat")
-    .optional()
-    .isFloat({ min: -90, max: 90 })
-    .withMessage("Maximum latitude is invalid"),
-  query("minLat")
-    .optional()
-    .isFloat({ min: -90, max: 90 })
-    .withMessage("Minimum latitude is invalid"),
-  query("minLng")
-    .optional()
-    .isFloat({ min: -180, max: 180 })
-    .withMessage("Minimum longitude is invalid"),
-  query("maxLng")
-    .optional()
-    .isFloat({ min: -180, max: 180 })
-    .withMessage("Maximum longitude is invalid"),
-  query("maxPrice")
-    .optional()
-    .isFloat({ min: 0 })
-    .withMessage("Maximum price must be greater than or equal to 0"),
-  query("minPrice")
-    .optional()
-    .isFloat({ min: 0 })
-    .withMessage("Minimum price must be greater than or equal to 0"),
-];
-
 const reviewValidator = (req, res, next) => {
   let { review, stars } = req.body;
   if (!review || !stars) {
@@ -507,6 +472,41 @@ router.get("/:spotId", requireAuth, async (req, res, next) => {
 });
 
 /***************** *    GET ALL SPOTS    *************************/
+
+const queryValidate = [
+  query("page")
+    .optional()
+    .isInt({ min: 1 })
+    .withMessage("Page must be greater than or equal to 1"),
+  query("size")
+    .optional()
+    .isInt({ min: 1 })
+    .withMessage("Page must be greater than or equal to 1"),
+  query("maxLat")
+    .optional()
+    .isFloat({ min: -90, max: 90 })
+    .withMessage("Maximum latitude is invalid"),
+  query("minLat")
+    .optional()
+    .isFloat({ min: -90, max: 90 })
+    .withMessage("Minimum latitude is invalid"),
+  query("minLng")
+    .optional()
+    .isFloat({ min: -180, max: 180 })
+    .withMessage("Minimum longitude is invalid"),
+  query("maxLng")
+    .optional()
+    .isFloat({ min: -180, max: 180 })
+    .withMessage("Maximum longitude is invalid"),
+  query("maxPrice")
+    .optional()
+    .isFloat({ min: 0 })
+    .withMessage("Maximum price must be greater than or equal to 0"),
+  query("minPrice")
+    .optional()
+    .isFloat({ min: 0 })
+    .withMessage("Minimum price must be greater than or equal to 0"),
+];
 
 router.get(
   "/",
