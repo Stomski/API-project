1806a813050cbe13fab1161df7c4230a33f90ddf
Robert Stomski <stomski.robert@gmail.com>
2024-04-28 14:07:12 -0600

    beggining sunday afternoon session

--- frontend/src/components/Navigation/Navigation-bonus.jsx
+++ frontend/src/components/Navigation/Navigation-bonus.jsx
@@ -5,7 +5,6 @@ import "./Navigation.css";
 import favicon from "../../../src/favicon.ico";
 import OpenModalButton from "../OpenModalButton";
 import CreateSpotModal from "../CreateSpotForm";
-import { useCallback } from "react";
 
 function Navigation({ isLoaded, navigate }) {
   // console.log(
